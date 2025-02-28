80f7aad0bfadc69718e13b876de09ca6fd0bd121
kjkombat <khuzaima.jamil@gmail.com>
2025-02-28 20:17:16 +1100

    added llm_generate_score function to score given resume text and get name.

--- llm.py
+++ llm.py
@@ -1,7 +1,9 @@
 import os
+from typing import List
 from dotenv import load_dotenv
 from mistralai import Mistral
-from pydantic import BaseModel
+from pydantic import BaseModel, create_model
+import re
 
 
 load_dotenv()
@@ -12,6 +14,10 @@ client = Mistral(api_key=os.environ["MISTRAL_API_KEY"])
 
 class ExtractedCriteria(BaseModel):
     criteria: list[str]
+
+
+class ExtracteUsername(BaseModel):
+    username: str
 
 
 def llm_response(job_description):
@@ -33,3 +39,41 @@ def llm_response(job_description):
     )
     response = chat_response.choices[0].message.parsed
     return response
+
+
+def llm_generate_score(job_resume, scoring_criteria):
+
+    scoring_criteria_cleaned = [
+        re.sub(r"[^a-zA-Z0-9]", "_", s) for s in scoring_criteria
+    ]
+    scoring_class = create_class_from_strings(scoring_criteria_cleaned, "ScoreResume")
+    system_prompt = f"""Given the parsed job resume. Return the following
+                  username: Extracted name of the candidate in format first name followed by a space then second name. If candidate name has more than two names then keep the same format just add space for each name.
+                    {"   ".join(f"{score}: Score candidate resume from 1 - 5 according to this criteria" for score in scoring_criteria_cleaned)}
+                  """
+    candidate_name = client.chat.parse(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": system_prompt,
+            },
+            {
+                "role": "user",
+                "content": job_resume,
+            },
+        ],
+        response_format=scoring_class,
+        max_tokens=256,
+        temperature=0,
+    )
+    response = candidate_name.choices[0].message.parsed
+    return response
+
+
+def create_class_from_strings(strings: List[str], class_name: str = "DynamicModel"):
+
+    field_definitions = {string: (str, "5") for string in strings}
+    field_definitions["username"] = (str, "")
+    dynamic_model = create_model(class_name, **field_definitions)
+    return dynamic_model
